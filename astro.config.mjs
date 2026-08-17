1e8a0775bd71baa71c4d1f43145d500e21ef1f32
羽毛 <hbnl555@qq.com>
2026-08-17 22:46:53 +0800

    大更新
    总之最大的更新就是做了一下切换界面的相关动效之类的，用ai随便生成了个赞助页面测试，后续要删掉重写，毕竟写的太随意了，我看着不舒服，剩下还有些小更新......不重要，反正基本都是点优化体验和让我看着舒服点的更新（绝对不是做了半天我发现我忘记更新啥了）

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,21 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import icon from "astro-icon";
+import swup from "@swup/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), icon()],
+  integrations: [
+    vue(),
+    icon(),
+    swup({
+      containers: ["#swup", "footer"],
+      smoothScrolling: true,
+      cache: true,
+      preload: true,
+      accessibility: true,
+      updateHead: true,
+      theme: false,
+    }),
+  ],
 });
